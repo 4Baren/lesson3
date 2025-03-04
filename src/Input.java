b81c9da63738c761ac1d4f454019fdd1a5c12fc3
4Baren <macbaren4@gmail.com>
2025-03-05 01:27:30 +0200

    Add lines

--- src/Input.java
+++ src/Input.java
@@ -6,5 +6,12 @@ public class Input {
         System.out.print("Input: ");
         String string = b.nextLine();
         System.out.println("It was " + string);
+//        Scanner c = new Scanner(System.in);
+        System.out.print("Input: ");
+        int x = b.nextInt();
+        System.out.println("It was: " + x);
+        System.out.print("Input: ");
+        double g = b.nextDouble();
+        System.out.println("It was: " + g);
     }
 }
